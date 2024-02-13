05315960b1c23fc9f4e498c99f67bf1d281153d6
Behnam Mozafari <bmoz0002@student.monash.edu>
2024-02-13 14:45:15 +1100

    debug

--- portfolio-website/app/components/EmailSection.jsx
+++ portfolio-website/app/components/EmailSection.jsx
@@ -8,7 +8,7 @@ const EmailSection = () => {
             <div className="absolute w-80 h-80 z-0 bg-[radial-gradient(ellipse_at_center,_var(--tw-gradient-stops))] from-blue-900 to-transparent rounded-full blur-lg top-[10%] left-1/2 transform -translate-x-3/4 translate-y-3/2"></div>
             <div className="col-span-12 z-10 flex flex-col items-center justify-center">
                 <h2 className="text-center text-4xl font-bold text-white my-2">
-                    <span className='text-transparent bg-clip-text bg-gradient-to-r from-white via-blue-200 to-blue-700'>Let's Connect</span>
+                    <span className='text-transparent bg-clip-text bg-gradient-to-r from-white via-blue-200 to-blue-700'>Let&#39;s Connect</span>
                 </h2>
                 <p className="text-center text-[#ADB7BE] mb-4 max-w-md">
                     I&#39;m currently looking for new opportunities, my inbox is always open.
